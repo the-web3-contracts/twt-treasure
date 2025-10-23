4d1e982964f08b1356037bb38e76a101cc0e8644
guoshijiang <guoshijiang2012@163.com>
2025-10-23 11:20:09 +0800

    add tr

---
+++ test/EmptyContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract EmptyContract {
+    function foo() public pure returns (uint256) {
+        return 0;
+    }
+}
---
+++ test/TreasureManagerTest.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TreasureManager} from "../src/TreasureManager.sol";
+
+contract TreasureManagerTest is Test {
+
+}
